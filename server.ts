70768297aaad459a56f9bc7eb2bc077a750f3a33
guirra-byte <guirramatheus1@gmail.com>
2024-07-22 20:36:24 -0300

    feat: initial commit

--- server.ts
+++
@@ -1,8 +0,0 @@
-import { app } from "./app";
-import { jpegifyRouter } from "./route";
-
-const PORT = 5433;
-app.use("/gdrive", jpegifyRouter);
-app.listen(PORT, () =>
-  console.log(`Server already is running on port ${PORT}`)
-);
