e77e1e173042f3b89d8758f79806458e24adddab
guirra-byte <guirramatheus1@gmail.com>
2024-08-02 17:49:27 -0300

    feat: add webhook endpoint to receive drive changes

--- source/route.ts
+++ source/route.ts
@@ -1,6 +1,8 @@
 import { Request, Response, Router } from "express";
 import { googleDriveService } from "./services/google-drive.service";
 import { JpegifyService } from "./services/jpegify.service";
+import { IChannelNotification } from "./types/watch-drive.types";
+import { WatchDriveService } from "./services/watch-drive.service";
 const jpegifyRouter = Router();
 
 export interface IJpegify {
@@ -11,17 +13,27 @@ export interface IJpegify {
   originLocation: string;
 }
 
-jpegifyRouter.post(
+jpegifyRouter.get(
   "/webhook/2jpeg",
   async (request: Request, response: Response) => {
-    const data: IJpegify = request.body;
+    const data: IChannelNotification = request.body;
     if (data) {
-      const dispatchToJpegifyService = new JpegifyService(googleDriveService);
-      await dispatchToJpegifyService.execute(data);
+      const watchDriveService = new WatchDriveService();
+      await watchDriveService.execute(data);
     }
 
-    return response.end();
+    return response.send();
   }
 );
+
+jpegifyRouter.post("/2jpeg", async (request: Request, response: Response) => {
+  const data: IJpegify = request.body;
+  if (data) {
+    const dispatchToJpegifyService = new JpegifyService(googleDriveService);
+    await dispatchToJpegifyService.execute(data);
+  }
+
+  return response.send();
+});
 
 export { jpegifyRouter };
