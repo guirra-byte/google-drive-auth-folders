7aced453e158b3e5ef798744a0fbd33e040926a3
guirra-byte <guirramatheus1@gmail.com>
2024-08-16 11:38:49 -0300

    fix: update app to parse json

--- source/server.ts
+++ source/server.ts
@@ -1,7 +1,13 @@
-import { app } from "./app";
+import "reflect-metadata";
+import { app } from "./shared/app";
 import { jpegifyRouter } from "./route";
+import express from "express";
 
 const PORT = 5433;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/gdrive", jpegifyRouter);
 app.listen(PORT, () =>
   console.log(`Server already is running on port ${PORT}`)
